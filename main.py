e7043038440b50704d29ab4d8f661b6d6863f112
StepanovTihon <mail2tihon@yandex.ru>
2022-03-08 18:19:10 +0400

    123

--- main.py
+++ main.py
@@ -77,14 +77,11 @@ def create_apartments(you_address):
 
 
 
-def read_apartments(you_address):
+def read_apartments(lodgers_id):
     cursor = conn.cursor()
 
-    cursor.execute(f"Select lodgers_id from apartments where address = '{you_address}'")
 
-    records = cursor.fetchall()
-
-    cursor.execute(f"Select name_lodgers from lodgers where lodgers_id = '{records[0][0]}'")
+    cursor.execute(f"Select address from apartments where lodgers_id = '{lodgers_id}'")
     records = cursor.fetchall()
     cursor.close()
 
@@ -107,7 +104,69 @@ def update_apartments(you_address, new_lodgers):
     return records
 
 
+def delete_apartments(you_address):
+    cursor = conn.cursor()
+
+    cursor.execute(f"Delete from apartments Where address = '{you_address}'")
+    conn.commit()
+    cursor.close()
+
+    conn.close()
+
+def create_lodgers(name_lodgers):
+    cursor = conn.cursor()
+
+    cursor.execute(f"Insert into lodgers(name_lodgers) values('{name_lodgers}');")
+    # records = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+
+    conn.close()
+
+
+def delete_lodgers(name_lodgers):
+    cursor = conn.cursor()
+
+    cursor.execute(f"Delete from lodgers Where name_lodgers = '{name_lodgers}'")
+    conn.commit()
+    cursor.close()
+
+    conn.close()
+
+def create_services(name_services, summ_of_payment, address, name_lodgers, date):
+    cursor = conn.cursor()
+
+
+
+    cursor.execute(f"Select apartments_id from apartments where address = '{address}'")
+    apartments_id = cursor.fetchall()[0][0]
+    cursor.execute(f"Select lodgers_id from lodgers where name_lodgers = '{name_lodgers}'")
+    lodgers_id = cursor.fetchall()[0][0]
+
+
+    cursor.execute(f"Insert into services(services_name,payment_amount,apartments_id,lodgers_id, date_services) values('{name_services}',{summ_of_payment},{apartments_id},{lodgers_id},'{date}');")
+    # records = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+
+    conn.close()
+
+def read_services(address, name_services, date):
+    cursor = conn.cursor()
+
+    cursor.execute(f"  lodgers_id from apartments where address = '{you_address}'")
+
+    records = cursor.fetchall()
+
+    cursor.execute(f"Select name_lodgers from lodgers where lodgers_id = '{records[0][0]}'")
+    records = cursor.fetchall()
+    cursor.close()
+
+    conn.close()
+    return records
+
+
 conn = psycopg2.connect(dbname="Base_Of_Tenants", user="postgres", password="pass2tihon", host="localhost")
-update_apartments("address99", "Tihon")
+create_services("water", 1000, "address3", "Tihon", "10-10-2022")
 conn.close()
 #print(records)
