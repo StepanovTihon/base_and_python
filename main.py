e62d6e48dffdd631755604202f6d74e802c7699b
StepanovTihon <mail2tihon@yandex.ru>
2022-04-28 20:28:10 +0400

    _

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import random
-
+import datetime
 import psycopg2
 from flask import Flask, request
 
@@ -131,53 +131,79 @@ def delete_lodgers(id):
 
     return "deletion completed"
 
-def create_services(name_services, summ_of_payment, address, name_lodgers, date):
-    cursor = conn.cursor()
+def create_services(name_services, summ_of_payment, apartments_id, lodgers_id, date):
+    conn = psycopg2.connect(dbname="Base_Of_Tenants", user="postgres", password="pass2tihon", host="localhost")
+
+    with conn:
+        with conn.cursor() as cursor:
 
 
 
-    cursor.execute(f"Select apartments_id from apartments where address = '{address}'")
-    apartments_id = cursor.fetchall()[0][0]
-    cursor.execute(f"Select lodgers_id from lodgers where name_lodgers = '{name_lodgers}'")
-    lodgers_id = cursor.fetchall()[0][0]
 
 
-    cursor.execute(f"Insert into services(services_name,payment_amount,apartments_id,lodgers_id, date_services) values('{name_services}',{summ_of_payment},{apartments_id},{lodgers_id},'{date}');")
-    # records = cursor.fetchall()
-    conn.commit()
-    cursor.execute(f"Select * from services where services_name = '{name_services}' AND lodgers_id = '{lodgers_id} AND date_services = '{date}'")
-    records = cursor.fetchall()
-    cursor.close()
 
-    conn.close()
+            cursor.execute(f"Insert into services(services_name,payment_amount,apartments_id,lodgers_id, date_services) values('{name_services}',{summ_of_payment},{apartments_id},{lodgers_id},'{date}');")
+            # records = cursor.fetchall()
+            conn.commit()
+            #cursor.execute(f"Select services_id, services_name,payment_amount,apartments_id,lodgers_id, date_services, paid from services where services_name = '{name_services}' AND lodgers_id = {lodgers_id} AND date_services = {date}")
+            #records = cursor.fetchall()
+
+    return "all good" #records
+ 
+def read_services(lodgers_id):
+    conn = psycopg2.connect(dbname="Base_Of_Tenants", user="postgres", password="pass2tihon", host="localhost")
+
+    with conn:
+        with conn.cursor() as cursor:
+
+
+
+            cursor.execute(f"Select services_name, payment_amount, date_services, paid, name_lodgers, lodgers.lodgers_id, address, apartments.apartments_id from services, lodgers, apartments  where services.lodgers_id = {lodgers_id} AND services.apartments_id = apartments.apartments_id AND lodgers.lodgers_id = {lodgers_id}")
+            records = cursor.fetchall()
+
     return records
 
-def read_services(lodgers_id):
-    cursor = conn.cursor()
+def read_all_services():
+    conn = psycopg2.connect(dbname="Base_Of_Tenants", user="postgres", password="pass2tihon", host="localhost")
+
+    with conn:
+        with conn.cursor() as cursor:
 
 
 
-    cursor.execute(f"Select services_name, payment_amount, date_services, paid, name_lodgers, address from services, lodgers, apartments  where services.lodgers_id = {lodgers_id} AND services.apartments_id = apartments.apartments_id")
-    records = cursor.fetchall()
-    cursor.close()
+            cursor.execute(f"Select services_name, payment_amount, date_services, paid, name_lodgers, lodgers.lodgers_id, address, apartments.apartments_id from services, lodgers, apartments  where services.apartments_id = apartments.apartments_id AND services.lodgers_id = lodgers.lodgers_id")
+            records = cursor.fetchall()
 
-    conn.close()
     return records
 
 
 def pay_services(lodgers_id, date, name_services):
-    cursor = conn.cursor()
+    conn = psycopg2.connect(dbname="Base_Of_Tenants", user="postgres", password="pass2tihon", host="localhost")
+
+    with conn:
+        with conn.cursor() as cursor:
 
 
 
 
-    cursor.execute(f"UPDATE services SET paid = True WHERE lodgers_id = {lodgers_id} AND date_services = '{date}' AND services_name = '{name_services}';")
-    # records = cursor.fetchall()
-    conn.commit()
-    cursor.close()
+            cursor.execute(f"UPDATE services SET paid = True WHERE lodgers_id = {lodgers_id} AND date_services = '{date}' AND services_name = '{name_services}';")
+            # records = cursor.fetchall()
+            conn.commit()
+            cursor.execute(f"Select services_name, payment_amount, date_services, paid, name_lodgers, lodgers.lodgers_id, address, apartments.apartments_id from services, lodgers, apartments  where services.lodgers_id = {lodgers_id} AND services.apartments_id = apartments.apartments_id AND lodgers.lodgers_id = {lodgers_id} AND date_services = '{date}' AND services_name = '{name_services}'")
+            records = cursor.fetchall()
 
-    conn.close()
-    return "service paid"
+    return records
+
+def delete_service(service_id):
+    conn = psycopg2.connect(dbname="Base_Of_Tenants", user="postgres", password="pass2tihon", host="localhost")
+
+    with conn:
+        with conn.cursor() as cursor:
+
+            cursor.execute(f"Delete from services Where services_id = '{service_id}'")
+            conn.commit()
+
+    return "deletion completed"
 
 def new_token(lodgers_id, token):
     conn = psycopg2.connect(dbname="Base_Of_Tenants", user="postgres", password="pass2tihon", host="localhost")
@@ -275,7 +301,7 @@ def get_home(id,token):
 
     to_json_keys = ['apartments_id', 'address']
     post_json = {}
-    resilt_json = "{"
+    resilt_json = "["
 
     for i in range(len(post)):
         for index, value in enumerate(post[i]):
@@ -284,7 +310,7 @@ def get_home(id,token):
         resilt_json += json.dumps(post_json) + ", "
         post_json.clear()
 
-    resilt_json = resilt_json[:len(resilt_json)-2]+"}"
+    resilt_json = resilt_json[:len(resilt_json)-2]+"]"
     return resilt_json
 
 @app.route('/delete_home/', methods=['POST'])
@@ -295,6 +321,107 @@ def delete_home():
     ret = delete_apartments(post["apartments_id"])
 
     return "all good"
+
+@app.route('/create_service/', methods=['POST'])
+def create_service():
+
+    post = request.json
+    if read_token(post["id"]) != post["token"]:
+        return "error please relogin"
+    ret=create_services(post["name_services"], post["summ_of_payment"], post["apartments_id"], post["id"], post["date"])[0]
+
+    to_json_keys = ['services_id', 'services_name', 'payment_amount', 'apartments_id', 'lodgers_id', 'date_services', 'paid']
+    post_json = {}
+    for index, value in enumerate(ret):
+        post_json[to_json_keys[index]] = value
+    return json.dumps(post_json)
+
+
+@app.route('/get_service/<int:id>/<int:token>')
+def get_service(id,token):
+
+    post = read_services(id)
+    if read_token(id) != token:
+        return "error please relogin"
+
+    to_json_keys = ['services_name', 'payment_amount', 'date_services', 'paid', 'name_lodgers', 'lodgers_id', 'address', 'apartments_id']
+    post_json = {}
+    resilt_json = "{"
+
+    for i in range(len(post)):
+        for index, value in enumerate(post[i]):
+            if index != 2:
+                post_json[to_json_keys[index]] = value
+            else:
+                post_json[to_json_keys[index]] = "'"+str(value)+"'"
+        resilt_json += json.dumps(post_json) + ", "
+        post_json.clear()
+
+    resilt_json = resilt_json[:len(resilt_json)-2]+"}"
+    print(resilt_json)
+    return resilt_json
+
+
+@app.route('/get_all_service/<int:id>/<int:token>')
+def get_all_service(id,token):
+
+    post = read_all_services()
+    if read_token(id) != token:
+        return "error please relogin"
+
+    to_json_keys = ['services_name', 'payment_amount', 'date_services', 'paid', 'name_lodgers', 'lodgers_id', 'address', 'apartments_id']
+    post_json = {}
+    resilt_json = "{["
+
+    for i in range(len(post)):
+        for index, value in enumerate(post[i]):
+            if index != 2:
+                post_json[to_json_keys[index]] = value
+            else:
+                post_json[to_json_keys[index]] = "'"+str(value)+"'"
+        resilt_json += json.dumps(post_json) + ", "
+        post_json.clear()
+
+    resilt_json = resilt_json[:len(resilt_json)-2]+"]}"
+    print(resilt_json)
+    return resilt_json
+
+
+@app.route('/pay_service/', methods=['POST'])
+def pay_service():
+
+    post = request.json
+    if read_token(post["id"]) != post["token"]:
+        return "error please relogin"
+    ret=pay_services(post["id"], post["date"], post["name_services"])
+    print(ret)
+    to_json_keys = ['services_name', 'payment_amount', 'date_services', 'paid', 'name_lodgers', 'lodgers_id', 'address', 'apartments_id']
+    post_json = {}
+    resilt_json = "{["
+
+    for i in range(len(ret)):
+        for index, value in enumerate(ret[i]):
+            if index != 2:
+                post_json[to_json_keys[index]] = value
+            else:
+                post_json[to_json_keys[index]] = "'" + str(value) + "'"
+        resilt_json += json.dumps(post_json) + ", "
+        post_json.clear()
+
+    resilt_json = resilt_json[:len(resilt_json) - 2] + "]}"
+    print(resilt_json)
+    return resilt_json
+
+
+@app.route('/delete_service/', methods=['POST'])
+def service_delete():
+    post = request.json
+    if read_token(post["id"]) != post["token"]:
+        return "error please relogin"
+    ret = delete_service(post["services_id"])
+
+    return "all good"
+
 
 @app.route("/")
 def hello():
