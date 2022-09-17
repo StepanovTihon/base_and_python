5245c7df7655c93f6b8826d9b8a8e42f1b6a543f
StepanovTihon <mail2tihon@yandex.ru>
2022-09-17 16:59:55 +0400

    /

--- BaseWorkspace.py
+++ BaseWorkspace.py
@@ -10,23 +10,67 @@ errors = {
 }
 
 
+def existence_apartments(id, you_address=''):
+    conn = psycopg2.connect(dbname=data[0], user=data[1], password=data[2], host=data[3])
+
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                f"Select * from apartments where address = '{you_address}'" if you_address != ''
+                else f"Select * from apartments Where apartments_id = '{id}'")
+
+            return cursor.fetchall()
+
+
+def existence_lodgers(id=-1, login=''):
+    conn = psycopg2.connect(dbname=data[0], user=data[1], password=data[2], host=data[3])
+
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(f"Select * from lodgers where login = '{login}'" if login != ''
+                           else f"Select * from lodgers Where lodgers_id = '{id}'")
+
+            return cursor.fetchall()
+
+
+def existence_services(name_services, date, lodgers_id, service_id=-1):
+    conn = psycopg2.connect(dbname=data[0], user=data[1], password=data[2], host=data[3])
+
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(f"Select * from services where services_name = '{name_services}' AND "
+                           f"date_services = '{date}' AND lodgers_id = {lodgers_id}" if service_id == -1
+                           else f"Select * from services where services_id = '{service_id}'")
+
+            return cursor.fetchall()
+
+
+def existence_indications(services_name, lodgers_id, date_indications):
+    conn = psycopg2.connect(dbname=data[0], user=data[1], password=data[2], host=data[3])
+
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(f"Select * from indications where services_name = '{services_name}' AND "
+                           f"date_indications = '{date_indications}' AND lodgers_id = {lodgers_id}")
+
+            return cursor.fetchall()
+
+
 def create_apartments(you_address):
     conn = psycopg2.connect(dbname=data[0], user=data[1], password=data[2], host=data[3])
 
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from apartments where address = '{you_address}'")
-
-            if len(cursor.fetchall()) != 0:
-                return [[], 400]
+            if len(existence_apartments(-1, you_address)) != 0:
+                return []
 
             cursor.execute(f"Insert into apartments(address) values('{you_address}');")
             conn.commit()
             cursor.execute(f"Select address, apartments_id from apartments where address = '{you_address}'")
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_apartments(apartments_id):
@@ -36,8 +80,8 @@ def read_apartments(apartments_id):
             cursor.execute(f"Select address from apartments where apartments_id = '{apartments_id}'")
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_all_apartments():
@@ -47,57 +91,40 @@ def read_all_apartments():
             cursor.execute(f"Select apartments_id, address from apartments")
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
-
-
-# def update_apartments(you_address, new_lodgers):
-#     cursor = conn.cursor()
-#     cursor.execute(f"Select lodgers_id from lodgers where name_lodgers = '{new_lodgers}'")
-#     records = cursor.fetchall()
-#     cursor.execute(f"UPDATE apartments SET lodgers_id = {records[0][0]} WHERE address= '{you_address}';")
-#     conn.commit()
-#     cursor.execute(f"Select address, name_lodgers from apartments, lodgers where apartments.lodgers_id = "
-#                    f"'{new_lodgers}' and lodgers.lodgers_id = '{new_lodgers}'")
-#     records = cursor.fetchall()
-#     cursor.close()
-#
-#     conn.close()
-#     return records
+            return records
+    return []
 
 
 def delete_apartments(apartments_id):
     conn = psycopg2.connect(dbname=data[0], user=data[1], password=data[2], host=data[3])
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from apartments where apartments_id = '{apartments_id}'")
-
-            if len(cursor.fetchall()) == 0:
-                return [[], 400]
+            if len(existence_apartments(apartments_id)) == 0:
+                return []
 
             cursor.execute(f"Delete from apartments Where apartments_id = '{apartments_id}'")
             conn.commit()
 
-            return [[], 200]
-    return [[], 500]
+            return []
+    return []
 
 
 def create_lodgers(name_lodgers, login, password):
     conn = psycopg2.connect(dbname=data[0], user=data[1], password=data[2], host=data[3])
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from lodgers where login = '{login}'")
-            if len(cursor.fetchall()) != 0:
-                return [[], 400]
+            if len(existence_lodgers(login)) != 0:
+                return []
             cursor.execute(
-                f"Insert into lodgers(name_lodgers,login,password, token) values('{name_lodgers}','{login}','{password}', {int(random.random() * 10000)});")
+                f"Insert into lodgers(name_lodgers,login,password, token) values('{name_lodgers}','{login}',"
+                f"'{password}', {int(random.random() * 10000)});")
             # records = cursor.fetchall()
             conn.commit()
             cursor.execute(
                 f"Select name_lodgers, lodgers_id, login, password, token from lodgers where name_lodgers = '{name_lodgers}'")
             records = cursor.fetchall()
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_lodgers(lodgers_id):
@@ -109,8 +136,8 @@ def read_lodgers(lodgers_id):
                            f"lodgers where lodgers.lodgers_id = '{lodgers_id}'")
             records = cursor.fetchall()
             conn.rollback()
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_all_lodgers():
@@ -123,8 +150,8 @@ def read_all_lodgers():
 
             conn.rollback()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def delete_lodgers(id):
@@ -132,15 +159,13 @@ def delete_lodgers(id):
 
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from lodgers where lodgers_id = '{id}'")
-
-            if len(cursor.fetchall()) == 0:
-                return [[], 400]
+            if len(existence_lodgers(-1, login)) == 0:
+                return []
             cursor.execute(f"Delete from lodgers Where lodgers_id = '{id}'")
             conn.commit()
 
-            return [[], 200]
-    return [[], 500]
+            return []
+    return []
 
 
 def create_services(name_services, summ_of_payment, apartments_id, lodgers_id, date):
@@ -148,10 +173,8 @@ def create_services(name_services, summ_of_payment, apartments_id, lodgers_id, d
 
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from services where services_name = '{name_services}' AND "
-                           f"date_services = '{date}' AND lodgers_id = {lodgers_id}")
-            if len(cursor.fetchall()) != 0:
-                return [[], 400]
+            if len(existence_services(name_services, date, lodgers_id)) != 0:
+                return []
             cursor.execute(
                 f"Insert into services(services_name,payment_amount,apartments_id,lodgers_id, date_services, paid) "
                 f"values('{name_services}',{summ_of_payment},{apartments_id},{lodgers_id},'{date}', false);")
@@ -160,8 +183,8 @@ def create_services(name_services, summ_of_payment, apartments_id, lodgers_id, d
 
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_services(lodgers_id):
@@ -175,8 +198,8 @@ def read_services(lodgers_id):
                            f"apartments.apartments_id AND lodgers.lodgers_id = {lodgers_id}")
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_all_services():
@@ -190,8 +213,8 @@ def read_all_services():
                            f"lodgers.lodgers_id")
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def pay_services(lodgers_id, date, name_services):
@@ -199,10 +222,8 @@ def pay_services(lodgers_id, date, name_services):
 
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from services where services_name = '{name_services}' AND "
-                           f"date_services = '{date}' AND lodgers_id = {lodgers_id}")
-            if len(cursor.fetchall()) == 0:
-                return [[], 400]
+            if len(existence_services(name_services, date, lodgers_id)) == 0:
+                return []
             cursor.execute(f"UPDATE services SET paid = True WHERE lodgers_id = {lodgers_id} AND date_services = "
                            f"'{date}' AND services_name = '{name_services}';")
             # records = cursor.fetchall()
@@ -214,8 +235,8 @@ def pay_services(lodgers_id, date, name_services):
                            f"'{date}' AND services_name = '{name_services}'")
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def delete_service(service_id):
@@ -223,15 +244,13 @@ def delete_service(service_id):
 
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from services where services_id = '{service_id}'")
-
-            if len(cursor.fetchall()) == 0:
-                return [[], 400]
+            if len(existence_services('', '', -1, service_id)) == 0:
+                return []
             cursor.execute(f"Delete from services Where services_id = '{service_id}'")
             conn.commit()
 
-            return [[], 200]
-    return [[], 500]
+            return []
+    return []
 
 
 def create_indications(services_name, apartments_id, lodgers_id, date_indications, value_indications):
@@ -239,10 +258,8 @@ def create_indications(services_name, apartments_id, lodgers_id, date_indication
 
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute(f"Select * from indications where services_name = '{services_name}' AND "
-                           f"date_indications = '{date_indications}' AND lodgers_id = {lodgers_id}")
-            if len(cursor.fetchall()) != 0:
-                return [[], 400]
+            if len(existence_indications(services_name, lodgers_id, date_indications)) != 0:
+                return []
             cursor.execute(
                 f"Insert into indications (services_name,value_indications,apartments_id,lodgers_id, date_indications) "
                 f"values('{services_name}',{value_indications},{apartments_id},{lodgers_id},'{date_indications}');")
@@ -253,8 +270,8 @@ def create_indications(services_name, apartments_id, lodgers_id, date_indication
                            f"lodgers_id = {lodgers_id} AND date_indications = '{date_indications}'")
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_indications(lodgers_id):
@@ -267,8 +284,8 @@ def read_indications(lodgers_id):
 
             records = cursor.fetchall()
 
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def new_token(lodgers_id, token):
@@ -282,8 +299,8 @@ def new_token(lodgers_id, token):
             conn.commit()
             cursor.execute(f"Select token, token_time from lodgers where lodgers_id = '{lodgers_id}'")
             records = cursor.fetchall()
-            return [records, 200]
-    return [[], 500]
+            return records
+    return []
 
 
 def read_token(lodgers_id):
@@ -294,5 +311,5 @@ def read_token(lodgers_id):
             records = cursor.fetchall()
             conn.commit()
 
-            return [records[0][0], 200]
-    return [[], 500]
+            return records[0][0]
+    return []
